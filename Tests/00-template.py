af57044a1ed132d61f71c1cee74254b4a37798d6
Richard Raymond <theplacetosendmemail@gmail.com>
2018-12-18 12:23:13 +0200

    Updated some stuff

--- Tests/00-template.py
+++ Tests/00-template.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 import sys
 
+result = 0
+error_info = "some sweet-ass error information"
+
+
+
+
+
+
+
 # UPDATE REPORT FILE
 reportfile = open(sys.argv[1], "a")
 reportfile.write('TEST:         [' + sys.argv[0] + ']\n')
-reportfile.write('RESULT:       [OK]')
+if result == "1":
+    reportfile.write('RESULT:       [OK]')
+else:
+    reportfile.write('RESULT:       [NOK]')
+    filename = sys.argv[1] + "_" + sys.argv[0]
+    errorfile = open(filename, "w+")
+    errorfile.write(error_info)
+    errorfile.close()
 reportfile.close()
--- monitor.py
+++ monitor.py
@@ -41,8 +41,8 @@ print('STRATOSCALE - QUICK MONITOR')
 # 1. Create report file
 now = datetime.datetime.now()
 filename = now.strftime("%Y%m%d_%H%M")
-filename = 'Reports/report-' + filename + '.txt'
-reportfile = open(filename, "w+")
+filename = 'report-' + filename
+reportfile = open('Reports/' + filename, "w+")
 reportfile.close()
 
 # 2. Iterate through test scripts
@@ -55,6 +55,6 @@ for i in scripts:
     print(dashes)
 
 # 3. Send report file to recipients
-reportfile = open(filename, "r")
+reportfile = open('Reports/' + filename, "r")
 print(reportfile.read())
 reportfile.close()
