631c1fa674afedbb85bb88b6e508bc8f1fb2dc13
Richard Raymond <theplacetosendmemail@gmail.com>
2018-12-19 14:17:22 +0200

    Updated some stuff

--- Tests/00-template.py
+++ Tests/00-template.py
@@ -21,6 +21,8 @@
 # v0.2 - 18 December 2018 (Richard Raymond)                                                                            #
 #   - YAML Config file handling.                                                                                       #
 #   - Added deeper error message customization (define error types in config.yaml)                                     #
+# v0.3 - 19 December 2018 (Richard Raymond)                                                                            #
+#   - Added better directory & error handling (directories are defined in the config.yml                               #
 #                                                                                                                      #
 ########################################################################################################################
 import sys
@@ -29,11 +31,18 @@ import yaml
 # PARAMETERS
 # 1 - Script name, 2 - Root path of calling script, 3 - Report filename
 
-# VARIABLES
-config = yaml.load(open(sys.argv[2] + '/config.yml', 'r'))                  # Pull in config information from YML file.
-result = 0                                                                  # Init OK/NOK marker
-error_message = "NO ERROR"                                                  # Error message to provide overview of issue
-error_data = "NO ERROR DATA PROVIDED"                                       # Full error contents
+# CONFIG VARIABLES
+rootpath = sys.argv[2]
+config = yaml.load(open( rootpath + '/config.yml', 'r'))                    # Pull in config information from YML file.
+testdirectory = rootpath + config['framework']['directory']['test']         # Generate directory string for test
+reportdirectory = rootpath + config['framework']['directory']['report']     # Generate directory string for reports
+workingdirectory = rootpath + config['framework']['directory']['working']   # Generate directory string for working dir
+scriptdirectory = rootpath + config['framework']['directory']['script']     # Generate directory string for sub scripts
+
+# SCRIPT VARIABLES
+result = 4                                                                  # Initialize OK/NOK marker
+error_message = "*UPDATE ME*"                                               # Error message to provide overview
+error_data = "*UPDATE ME*"                                                  # Full error contents
 # ----------------------------------------------------------------------------------------------------------------------
 # TEST SCRIPT DATA GOES HERE
 
@@ -48,15 +57,15 @@ error_data = "NO ERROR DATA PROVIDED"                                       # Fu
 
 # ----------------------------------------------------------------------------------------------------------------------
 # UPDATE REPORT FILE
-reportfile = open(sys.argv[2] + '/Reports/' + sys.argv[3] + '.txt', "a")    # Open the current report file
+reportfile = open(reportdirectory + sys.argv[3] + '.txt', "a")              # Open the current report file
 reportfile.write('TEST:         ' + sys.argv[1] + '\n')                     # Open test section in report file
-reportfile.write('RESULT:       ' + config['errortypes'][result])           # Add test status
+reportfile.write('RESULT:       ' + config['errortypes'][result])           # Add test status to report
 if result != 0:                                                             # Check if test wasn't successful
     errorfilename = sys.argv[3] + "_" + sys.argv[1]                         # Create a error_reportfile
-    errorfile = open(sys.argv[2] + '/Reports/' + errorfilename + '.txt', "w+")  # Create error report file
+    errorfile = open(reportdirectory + errorfilename + '.txt', "w+")        # Create error report file
     errorfile.write(error_data)                                             # Write error data to error file
     errorfile.close()                                                       # Close error file
     reportfile.write(" : " + error_message + '\n')                          # Add error message to report
     reportfile.write(" Please look at [" + errorfilename + ".txt] for further details.")
-reportfile.write('\n' + config['linebreak'] + '\n')
+reportfile.write('\n' + config['formatting']['linebreak'] + '\n')           # Add line break to report file, after test
 reportfile.close()                                                          # Close report file
--- monitor.py
+++ monitor.py
@@ -26,19 +26,23 @@
 # v0.2 - 18 December 2018 (Richard Raymond)                                                                            #
 #   - Added improve report file handling and additional parameters for script calls (script name, root path and report)#
 #   - Reads to the report file to output, rather than prining output direct console                                    #
+# v0.3 - 19 December 2018 (Richard Raymond)                                                                            #
+#   - Added better directory & error handling (directories are defined in the config.yml                               #
 #                                                                                                                      #
 ########################################################################################################################
 
 # MODULES
 import sys
-import os
-import datetime
-import subprocess
+import os                                                               # For os modules
+import datetime                                                         # For dates and time []
+import subprocess                                                       # For running os commands
 import yaml                                                             # For reading the config file
 
 # VARIABLES
 rootpath = os.getcwd()                                                  # Get the root path
-config = yaml.load(open(rootpath + '/config.yml', 'r'))                 # Pull in config information from YML file
+config = yaml.load(open('config.yml', 'r'))                             # Pull in config information from YML file
+testdirectory = rootpath + config['framework']['directory']['test']     # Generate directory string for test
+reportdirectory = rootpath + config['framework']['directory']['report']  # Generate directory string for reports
 
 # SPLASH SCREEN
 print(sys.path)
@@ -48,19 +52,20 @@ print('STRATOSCALE - QUICK MONITOR')
 now = datetime.datetime.now()                                           # Get the date & time (for the filename)
 reportfilename = now.strftime("%Y%m%d_%H%M")                            # Create the filename
 reportfilename = 'report-' + reportfilename                             # Append the prefix [report-] to the filename
-reportfile = open(rootpath + '/Reports/' + reportfilename + '.txt', "w+")  # Create a report file.
-reportfile.write('STATUS REPORT\n' + config['linebreak'] + '\n')
+reportfile = open(reportdirectory + reportfilename + '.txt', "w+")      # Create a report file.
+reportfile.write(config['region']['region1']['name'])
+reportfile.write('\nSTATUS REPORT\n' + config['formatting']['linebreak'] + '\n')
 reportfile.close()                                                      # Close the report file for later editing.
 
 # RUN THROUGH ALL TESTS
-scripts = os.listdir(rootpath + '/Tests')                               # Get the list of scripts in the tests folder
+scripts = os.listdir(testdirectory)                                     # Get the list of scripts in tests folder
 for i in scripts:                                                       # For each script DO
-    subprocess.call(['python', 'Tests/' + i, i[:-3], rootpath, reportfilename])  # Run the test script
+    subprocess.call(['python', testdirectory + i, i[:-3], rootpath, reportfilename])  # Run the test script
 
 # READ OUT TEST RESULTS, COMPRESS AND EMAIL RESULTS
-reportfile = open(rootpath + '/Reports/' + reportfilename + '.txt', "r")  # Open the report file
+reportfile = open(reportdirectory + reportfilename + '.txt', "r")       # Open the report file
 print(reportfile.read())                                                # Read rge report file out to the user.
 reportfile.close()                                                      # Close the report file for later editing.
 
-# TODO - Zip relevant report files
-# TODO - Email zipped report data to addresses in recipients
+# TODO - v0.4 - Zip relevant report files
+# TODO - v0.4 - Email zipped report data to addresses in recipients
